21eacdec22839bc961f1f5727866eab91b3a3453
Václav Lukeš <vasek@flexsys.cz>
2023-10-05 10:35:17 +0200

    changed FormValues from interface to helper class

--- src/FormValues.php
+++ src/FormValues.php
@@ -3,7 +3,26 @@ declare(strict_types=1);
 
 namespace Flexsyscz\Forms;
 
+use Nextras\Dbal\Utils\DateTimeImmutable;
 
-interface FormValues
+
+class FormValues
 {
+	public static function toDateTime(string $value, string $format = null): DateTimeImmutable
+	{
+		$formats = $format ? [$format] : [
+			'j. n. Y',
+			'j. n. Y H:i',
+			'j. n. Y H:i:s'
+		];
+
+		foreach ($formats as $format) {
+			$dateTime = DateTimeImmutable::createFromFormat($format, $value);
+			if($dateTime) {
+				return new DateTimeImmutable($dateTime->format('Y-m-d H:i:s'));
+			}
+		}
+
+		throw new InvalidArgumentException(sprintf('Cannot create instance from value %s.', $value));
+	}
 }
---
+++ src/InvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flexsyscz\Forms;
+
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}

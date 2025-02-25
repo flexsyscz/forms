a5b3e2599d979df32925a7b4ed016e499bcf97d0
Václav Lukeš <vasek@flexsys.cz>
2025-02-25 15:00:18 +0100

    add FormFactory

---
+++ src/FormFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flexsyscz\Forms;
+
+use Nette;
+
+
+abstract class FormFactory
+{
+	public function create(callable $onRender = null): Nette\Application\UI\Form
+	{
+		$form = new Nette\Application\UI\Form();
+		$form->onRender[] = $onRender ?? Nette\Utils\Callback::check([$this, 'onRender']);
+
+		return $form;
+	}
+
+
+	abstract public function onRender(Nette\Application\UI\Form $form): void;
+}
